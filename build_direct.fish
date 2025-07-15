e77d9373c3affd3648fd12d6dde92383c0befa94
james-l-key <jameslkey@gmail.com>
2025-07-15 22:25:18 +0000

    🗑️ Remove unused Fish scripts for ESP-IDF build and environment setup

--- build_direct.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/fish
-source /home/phi/esp/v5.4.2/esp-idf/export.fish
-set -x IDF_TOOLCHAIN clang
-set -x CC /home/phi/.espressif/tools/esp-clang/esp-18.1.2_20240912/esp-clang/bin/clang
-set -x CXX /home/phi/.espressif/tools/esp-clang/esp-18.1.2_20240912/esp-clang/bin/clang++
-cd examples/basic_menu
-idf.py build -DCMAKE_C_FLAGS="-Wno-logical-not-parentheses"
